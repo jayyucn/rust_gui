73cab1049d6063fd5fec7db52162c2e4424cbc56
nalanrola <nalanrola@gmail.com>
2021-06-26 01:02:17 +0800

    test ui

--- gui_druid/README.md
+++ gui_druid/README.md
@@ -1,2 +1,4 @@
 Here is a [tutorial documentation](https://linebender.org/druid/)
 
+This demo is running on:
+`cargo 1.52.0 (69767412a 2021-04-21)`
--- gui_druid/src/main.rs
+++ gui_druid/src/main.rs
@@ -1,8 +1,19 @@
-use druid::widget::Label;
-use druid::{AppLauncher, PlatformError, Widget, WindowDesc};
+#![windows_subsystem = "windows"]
+use druid::widget::{Flex, Label};
+use druid::{AppLauncher, Color, PlatformError, Widget, WindowDesc};
 
 fn build_ui() -> impl Widget<()> {
-    Label::new("Hello world")
+    Flex::row().with_flex_child(
+        Flex::column()
+            .with_child(Label::new("Hello, ").with_text_color(Color::rgba(
+                1.0,
+                102.0 / 255.0,
+                94.0 / 255.0,
+                0.5,
+            )))
+            .with_child(Label::new("World")),
+        1.0,
+    )
 }
 
 fn main() -> Result<(), PlatformError> {
